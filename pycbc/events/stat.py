36c949c2531336d9ed4bb44a9dbdc58fa47ae2bb
Sam Insley <samlinsley97@gmail.com>
2026-01-07 00:18:13 -0800

    Background checks

--- pycbc/events/stat.py
+++ pycbc/events/stat.py
@@ -1585,7 +1585,7 @@ class ExpFitFgBgNormStatistic(PhaseTDStatistic,
         # Noise PDF is 1/volume, assuming a uniform distribution of noise
         # coincs
         logr_n = - numpy.log(hist_vol)
-
+        print(logr_n, noise_twindow, self.srbmax, self.srbmin, self.swidth, n_ifos)
         # Combine to get final statistic: log of
         # ((rate of signals / rate of noise) * PTA Bayes factor)
         loglr = network_logvol - ln_noise_rate + logr_s - logr_n
@@ -1671,7 +1671,7 @@ class ExpFitFgBgNormStatistic(PhaseTDStatistic,
         # Noise PDF is 1/volume, assuming a uniform distribution of noise
         # coincs
         logr_n = - numpy.log(hist_vol)
-
+        print(logr_n, noise_twindow, self.srbmax, self.srbmin, self.swidth, n_ifos)
         loglr = - thresh + network_logvol - ln_noise_rate + logr_s - logr_n
         loglr += self.stat_correction
         return loglr
