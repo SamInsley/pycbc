3bdbfc3c7606719ce1cbbbb4ff8ee36418926815
Ian Harry <ian.harry@ligo.org>
2026-02-09 21:47:03 +0000

    V2315 changes (#5278)
    * Bump version
    
    * Move to pegasus 3.1.2
    
    * Support for pegasus v5.1.1 (#5142)
    
    * Update to pegasus 5.1.1
    
    * FIXUP
    
    * Pegasus no longer shipping images for old ubuntu - update
    
    * Think we need to remove this as well
    
    * Must constrain dynesty
    
    * Add extra requirement to pegasus_sites.py
    
    * Correct pegasus requirements
    
    * Don't want this failing due to missing dependencies
    
    * Try lower mpl version
    
    * This is what 3.14 had
    
    * Ignore some >= 2025 dependencies
    
    * The tutorials need a newer version, no need to run them here
    
    * Put this back
    
    * ARRGGGHH! Setuptools 82 breaks everything. Avoid

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ install_requires = setup_requires + [
     'tqdm',
     'setuptools<82.0.0',
     'gwdatafind',
-    'pegasus-wms.api >= 5.0.6',
+    'pegasus-wms.api == 5.1.2',
     'python-ligo-lw >= 1.7.0',
     'ligo-segments',
     'lalsuite!=7.2,<7.25',
@@ -119,7 +119,7 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '2.3.14'
+        vinfo.version = '2.3.15'
         vinfo.release = True
 
     version_script = f"""# coding: utf-8
